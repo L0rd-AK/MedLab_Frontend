7251eabcb56a14b6a03b12484fa8088a062c315a
Amit Kumar <aka883207@gmail.com>
2024-01-24 18:52:14 +0600

    project almost finished

--- src/components/Dashboard/AppointmentCard.jsx
+++ src/components/Dashboard/AppointmentCard.jsx
@@ -1,7 +1,39 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AppointmentCard = ({test}) => {
     const {_id,testName,imageUrl,price,date,slots}=test;
+    const cancleAppointment=(id)=>{
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              fetch(`https://backend-server-gamma.vercel.app/appointments/${id}`, {
+                method: "DELETE",
+                headers: {
+                    'content-type': 'application/json'
+                },
+              })
+              .then(res=>res.json())
+              .then(data=>{
+                if(data.deletedCount > 0){
+                    Swal.fire(
+                        'Deleted!',
+                        'Test has been deleted.',
+                        'success'
+                      )
+                    location.reload();
+                }
+              })
+            }
+          })
+    }
     return (
         <tbody className="text-center">
             {/* row 1 */}
@@ -29,7 +61,7 @@ const AppointmentCard = ({test}) => {
                 </td>
                 <td>{slots}</td>
                 <th>
-                    <Link to={`/all-tests/${_id}`}><button className="btn btn-ghost btn-lg BgPrimary">cancle</button></Link>
+                    <Link onClick={()=>cancleAppointment(_id)}><button className="btn btn-ghost btn-lg BgPrimary">cancle</button></Link>
                 </th>
             </tr>
         </tbody>
